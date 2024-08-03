eee47ca20937800aa13e9c574c8bad9b7fa6a455
sehdevkumar <kumarsehdev600@gmail.com>
2024-08-03 11:36:19 +0530

    done changes

--- src/views/Orders.tsx
+++ src/views/Orders.tsx
@@ -1,6 +1,17 @@
+import OrdersTable from "./Orders/OrdersTable";
 
 export default function Orders() {
   return (
-    <div>Orders</div>
+     
+     <div className="grid gap-4 justify-between p-4 sm:grid-cols-1 md:grid-cols-1  lg:grid-cols-2 xl:grid-cols-2">
+        <div className="flex flex-wrap flex-1 gap-4 w-full">
+        <OrdersTable/>
+        </div>
+        <div className="flex min-w-max justify-center md:justify-start">
+          <div className="w-full max-w-full flex flex-1">
+          </div>
+        </div>
+      </div>
+
   )
 }
